37a31315a205df56ddda3cca2017e4a1225e4e3f
Rafael Karato <rkarato@gmail.com>
2021-05-02 11:03:25 +0200

    initial commit test_calc.py

---
+++ test_calc.py
@@ -0,0 +1,10 @@
+import calc
+
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert 4 == calc.add(2, 2)
+
+    def test_subtraction(self):
+        assert 2 == calc.subtract(4, 2)
